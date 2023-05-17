07a15817efa6f4f35fe137c937d486727fe6e034
erazemp <ep7525@student.uni-lj.si>
2023-05-17 22:00:14 +0200

    kalorije

--- pedometer/pedometer.ino
+++ pedometer/pedometer.ino
@@ -27,6 +27,7 @@ const char *blynk_auth_token = "DZcBJHgpw8rjOJGr0kz9MuMi8C415dlp";
 
 Ticker tick_beri;
 Ticker tick_reset;
+Ticker tick_calroies;
 int32_t table_x[TABLE_SIZE_MPU];
 int32_t table_y[TABLE_SIZE_MPU];
 int32_t table_z[TABLE_SIZE_MPU];
@@ -34,6 +35,8 @@ int32_t SMOOTHING_WINDOW = 2;
 int32_t history_x[HISTORY_SIZE];
 int32_t history_y[HISTORY_SIZE];
 int32_t history_z[HISTORY_SIZE];
+int32_t calorie_stepCounter = 0;
+float total_calories_burned = 0.0;
 int32_t stepCounter = 0;
 float delilnik = 16384.0f;
 float acc_x_calib = 0.0f;
@@ -43,6 +46,10 @@ float acc_z_calib = 0.0f;
 float threshold = 1000000.0f;
 int32_t countsSinceLastStep = 0;
 
+// izmisljeni parametri
+float weight = 85.0;
+int32_t height = 178;
+
 // funkcije:
 void beriPodatke();
 
@@ -50,7 +57,73 @@ void resetDailySteps();
 
 void acc_config();
 
-int32_t preveriNajvecjoOs() {
+float get_stride(int32_t Nsteps, int32_t height)
+{
+
+  if (Nsteps == 1)
+  {
+    return height / 5;
+  }
+  else if (Nsteps == 2)
+  {
+    return height / 4;
+  }
+  else if (Nsteps == 3)
+  {
+    return height / 3;
+  }
+  else if (Nsteps == 4)
+  {
+    return height / 2;
+  }
+  else if (Nsteps == 5)
+  {
+    return height / 1.2;
+  }
+  else if (Nsteps == 6 || Nsteps == 7)
+  {
+    return height;
+  }
+  else if (Nsteps >= 8)
+  {
+    return 1.2 * height;
+  }
+}
+
+void kalorije_poraba(int32_t nmb_of_steps)
+{
+  //  2 options: 1. stationary, 2. moving
+  //  stationary: C = 1 * weight/1800
+  //  moving (2 second update): C = speed * weight/400
+  //  speed = steps per 2s * stride/2s
+  //  stride = table of steps per 2s --> height/5, 4, 3, 2, 1.2, Height, 1.2*Height
+  // Height is in cm, weight in kg
+  int32_t nmb_of_steps_in_last_2s = nmb_of_steps - calorie_stepCounter;
+  float current_calories_burned = 0.0;
+
+  if (nmb_of_steps_in_last_2s == 0)
+  {
+    // stationary
+    current_calories_burned = weight / 1800;
+
+    // sporoci na dashboard
+  }
+  else
+  {
+
+    float stride = get_stride(nmb_of_steps_in_last_2s, height);
+
+    float speed = nmb_of_steps_in_last_2s * stride;
+    current_calories_burned = speed * weight / 400;
+
+    // sporoci na dashboard
+  }
+
+  calorie_stepCounter += nmb_of_steps;
+}
+
+int32_t preveriNajvecjoOs()
+{
   // 0 = x os
   // 1 = y os
   // 2 = z os
@@ -59,29 +132,36 @@ int32_t preveriNajvecjoOs() {
   int32_t max_y = 0;
   int32_t max_z = 0;
   // begin count
-  for (int i = 0; i < HISTORY_SIZE; i++) {
-    if (abs(history_x[i] > max_x)) {
+  for (int i = 0; i < HISTORY_SIZE; i++)
+  {
+    if (abs(history_x[i] > max_x))
+    {
       max_x = abs(history_x[i]);
     }
-    if (abs(history_y[i] > max_y)) {
+    if (abs(history_y[i] > max_y))
+    {
       max_y = abs(history_y[i]);
     }
-    if (abs(history_z[i] > max_z)) {
+    if (abs(history_z[i] > max_z))
+    {
       max_z = abs(history_z[i]);
     }
   }
   // get maximum
-  if (max_x > max_y && max_x > max_z) {
+  if (max_x > max_y && max_x > max_z)
+  {
     return 0;
-  } else if (max_y > max_x && max_y > max_z) {
+  }
+  else if (max_y > max_x && max_y > max_z)
+  {
     return 1;
   }
   // default, vrni z
   return 2;
 }
 
-
-void beriPodatke() {
+void beriPodatke()
+{
   static uint32_t count = 0;
   digitalWrite(PIN_LED, 0);
   static float acc_x = 0.0f;
@@ -93,6 +173,7 @@ void beriPodatke() {
 
   //**** MPU-9250
   //**** Naslov registra
+  //**** Naslov registra
   // "zapiši", od katerega naslova registra dalje želimo brati
   Wire.beginTransmission(I2C_ADD_MPU);
   Wire.write(ACC_OUT);
@@ -101,21 +182,31 @@ void beriPodatke() {
   //** Branje: pospešek
   //** Zdaj mikrokrmilnik bere od naslova ACC_OUT
   //** Bere vseh 6 bajtov (x, y in z os):
+  //** Bere vseh 6 bajtov (x, y in z os):
   Wire.requestFrom(I2C_ADD_MPU, 6);
-  for (int i = 0; i < 6; i++) {
-    if (i < 2) {
-      table_x = (int8_t) Wire.read();
-      if (i % 2 == 0) {
+  for (int i = 0; i < 6; i++)
+  {
+    if (i < 2)
+    {
+      table_x = (int8_t)Wire.read();
+      if (i % 2 == 0)
+      {
         table_x = table_x << 8;
       }
-    } else if (i < 4) {
-      table_y = (int8_t) Wire.read();
-      if (i % 2 == 0) {
+    }
+    else if (i < 4)
+    {
+      table_y = (int8_t)Wire.read();
+      if (i % 2 == 0)
+      {
         table_y = table_y << 8;
       }
-    } else {
-      table_z = (int8_t) Wire.read();
-      if (i % 2 == 0) {
+    }
+    else
+    {
+      table_z = (int8_t)Wire.read();
+      if (i % 2 == 0)
+      {
         table_z = table_z << 8;
       }
     }
@@ -126,7 +217,8 @@ void beriPodatke() {
   acc_y += ((table_y / delilnik) - acc_y_calib) / RATE;
   acc_z += ((table_z / delilnik) - acc_z_calib) / RATE;
 
-  if (count % RATE == 0) {
+  if (count % RATE == 0)
+  {
     // Izpišemo
     Serial.print("ACC_X: X= ");
     Serial.print(acc_x);
@@ -155,7 +247,8 @@ void beriPodatke() {
     acc_z = 0;
   }
 
-  if (count == HISTORY_SIZE) {
+  if (count == HISTORY_SIZE)
+  {
     Serial.print("START DETECTION STEP");
     Serial.println("");
     // glajenje (vzemi prejšnji, trenutni in naslednji measurment in vstavi povprečje
@@ -165,10 +258,13 @@ void beriPodatke() {
     int32_t number_summed_x = 0;
     int32_t number_summed_y = 0;
     int32_t number_summed_z = 0;
-    for (int i = 0; i < SMOOTHING_WINDOW; i++) {
+    for (int i = 0; i < SMOOTHING_WINDOW; i++)
+    {
       int32_t start_index = i - SMOOTHING_WINDOW;
-      for (int smoothing_index = i - SMOOTHING_WINDOW; smoothing_index < i + SMOOTHING_WINDOW; smoothing_index++) {
-        if (smoothing_index < 0 || smoothing_index > HISTORY_SIZE) {
+      for (int smoothing_index = i - SMOOTHING_WINDOW; smoothing_index < i + SMOOTHING_WINDOW; smoothing_index++)
+      {
+        if (smoothing_index < 0 || smoothing_index > HISTORY_SIZE)
+        {
           continue;
         }
         summed_x += history_x[i];
@@ -186,82 +282,97 @@ void beriPodatke() {
       history_z[i] = average_z;
     }
 
-    //TODO: dinamično nastavljanje meje
+    // TODO: dinamično nastavljanje meje
     int32_t najvecjaOs = preveriNajvecjoOs();
     int32_t maxHistory[HISTORY_SIZE];
-    if (najvecjaOs == 0) {
+    if (najvecjaOs == 0)
+    {
       memcpy(maxHistory, history_x, HISTORY_SIZE);
-    } else if (najvecjaOs == 1) {
+    }
+    else if (najvecjaOs == 1)
+    {
       memcpy(maxHistory, history_y, HISTORY_SIZE);
-    } else if (najvecjaOs == 2) {
+    }
+    else if (najvecjaOs == 2)
+    {
       memcpy(maxHistory, history_z, HISTORY_SIZE);
     }
     // get max and min values
     int max_value = INT_MIN;
     int min_value = INT_MAX;
-    for (int i = 0; i < HISTORY_SIZE; i++) {
-      if (maxHistory[i] > max_value) {
+    for (int i = 0; i < HISTORY_SIZE; i++)
+    {
+      if (maxHistory[i] > max_value)
+      {
         max_value = maxHistory[i];
       }
-      if (maxHistory[i] < min_value) {
+      if (maxHistory[i] < min_value)
+      {
         min_value = maxHistory[i];
       }
     }
-    // step detection
-    for (int i = 1; i < HISTORY_SIZE; i++) {
-      int32_t previous = maxHistory[i - 1];
-      int32_t current = maxHistory[i];
-      if (current < previous && previous > threshold && current < threshold && countsSinceLastStep > 2) {
-        // todo: upostevaj se cas med obema korakom (periodicnost!!!)
-        Serial.print("STEP DETECTED");
-        Serial.println("");
-        stepCounter++;
-        countsSinceLastStep = 0;
+  }
+  // step detection
+  for (int i = 1; i < HISTORY_SIZE; i++)
+  {
+    int32_t previous = maxHistory[i - 1];
+    int32_t current = maxHistory[i];
+    if (current < previous && previous > threshold && current < threshold && countsSinceLastStep > 2)
+    {
+      // todo: upostevaj se cas med obema korakom (periodicnost!!!)
+      Serial.print("STEP DETECTED");
+      Serial.println("");
+      stepCounter++;
+      countsSinceLastStep = 0;
 
-        Serial.print("Publishing message on V3: ");
-        Serial.println(stepCounter);
-        Blynk.virtualWrite(V3, stepCounter);
+      Serial.print("Publishing message on V3: ");
+      Serial.println(stepCounter);
+      Blynk.virtualWrite(V3, stepCounter);
 
-        // Preveri, ali je bil dosežen dnevni cilj korakov (5000 korakov)
-        if (stepCounter >= 5000) {
-          Serial.print("Publishing message on V5: ");
-          Serial.println("Daily step goal reached!");
-          Blynk.virtualWrite(V5, "Daily step goal reached!");
-        } else {
-          Serial.print("Publishing message on V4: ");
-          Serial.println("Daily step goal not yet reached!");
-          Blynk.virtualWrite(V5, "Daily step goal not yet reached!");
-        }
+      // Preveri, ali je bil dosežen dnevni cilj korakov (5000 korakov)
+      if (stepCounter >= 5000)
+      {
+        Serial.print("Publishing message on V5: ");
+        Serial.println("Daily step goal reached!");
+        Blynk.virtualWrite(V5, "Daily step goal reached!");
+      }
+      else
+      {
+        Serial.print("Publishing message on V4: ");
+        Serial.println("Daily step goal not yet reached!");
+        Blynk.virtualWrite(V5, "Daily step goal not yet reached!");
       }
     }
-
-    // set new threshold
-    threshold = (max_value + min_value) / 2;
-
-    //TODO: count calories
-
-    // reset counter
-    count = 0;
   }
-  history_x[count] = acc_x;
-  history_y[count] = acc_y;
-  history_z[count] = acc_z;
 
-  // števec
-  count = count + 1;
-  countsSinceLastStep++;
-  //digitalWrite(PIN_LED, 1);
+  // set new threshold
+  threshold = (max_value + min_value) / 2;
+
+  // TODO: count calories
+
+  // reset counter
+  count = 0;
+}
+history_x[count] = acc_x;
+history_y[count] = acc_y;
+history_z[count] = acc_z;
+
+// števec
+count = count + 1;
+countsSinceLastStep++;
+// digitalWrite(PIN_LED, 1);
 }
 
-void resetStepCount() {
+void resetStepCount()
+{
   stepCounter = 0;
   Serial.println("Step count reset!");
 }
 
+void acc_calib()
+{
 
-void acc_calib() {
-
-  //digitalWrite(PIN_LED, 0);
+  // digitalWrite(PIN_LED, 0);
 
   delay(1000);
 
@@ -273,27 +384,37 @@ void acc_calib() {
 
   //**** MPU-9250
   // "zapiši", od katerega naslova registra dalje želimo brati
-  for (int q = 0; q < samp; q++) {
+  for (int q = 0; q < samp; q++)
+  {
     Wire.beginTransmission(I2C_ADD_MPU);
     Wire.write(ACC_OUT);
     Wire.endTransmission();
 
     //** Branje: pospeškometera
     Wire.requestFrom(I2C_ADD_MPU, 6);
-    for (int i = 0; i < 6; i++) {
-      if (i < 2) {
-        table_x = (int8_t) Wire.read();
-        if (i % 2 == 0) {
+    for (int i = 0; i < 6; i++)
+    {
+      if (i < 2)
+      {
+        table_x = (int8_t)Wire.read();
+        if (i % 2 == 0)
+        {
           table_x = table_x << 8;
         }
-      } else if (i < 4) {
-        table_y = (int8_t) Wire.read();
-        if (i % 2 == 0) {
+      }
+      else if (i < 4)
+      {
+        table_y = (int8_t)Wire.read();
+        if (i % 2 == 0)
+        {
           table_y = table_y << 8;
         }
-      } else {
-        table_z = (int8_t) Wire.read();
-        if (i % 2 == 0) {
+      }
+      else
+      {
+        table_z = (int8_t)Wire.read();
+        if (i % 2 == 0)
+        {
           table_z = table_z << 8;
         }
       }
@@ -321,7 +442,8 @@ void acc_calib() {
   delay(1000);
 }
 
-void setup_wifi() {
+void setup_wifi()
+{
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -331,7 +453,8 @@ void setup_wifi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -344,7 +467,8 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void setup() {
+void setup()
+{
   // Initialize the BUILTIN_LED pin as an output
   pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(115200);
@@ -354,14 +478,18 @@ void setup() {
 
   // Inicializiramo I2C na podanih pinih
   Wire.begin(12, 14);
+  Wire.begin(12, 14);
   // nastavimo frekvenco vodila na 100 kHz
   Wire.setClock(100000);
 
   acc_calib();
   tick_beri.attach_ms(INTERVAL_BERI, beriPodatke);
   tick_reset.attach(INTERVAL_RESET, resetStepCount);
+  // calorie ticker
+  tick_calroies.attach_ms(2000, kalorije_poraba(stepCounter));
 }
 
-void loop() {
+void loop()
+{
   Blynk.run();
 }
